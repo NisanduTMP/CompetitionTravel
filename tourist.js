0b7d409570a0d47bd77314961553af28db7d06dc
methsilusenavirathne <methsilusenavirathne@outlook.com>
2024-06-02 08:49:19 +0530

    attractions-page

---
+++ tourist.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let quotes = document.querySelectorAll('#quoteContainer .quote');
+    let currentQuoteIndex = 0;
+
+    function showNextQuote() {
+        quotes[currentQuoteIndex].classList.remove('show');
+
+        setTimeout(function() {
+            quotes[currentQuoteIndex].style.display = 'none';
+
+            let nextQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+            quotes[nextQuoteIndex].style.display = 'flex';
+
+            setTimeout(function() {
+                quotes[nextQuoteIndex].classList.add('show');
+                currentQuoteIndex = nextQuoteIndex;
+            }, 20);
+        }, 1000);
+
+    }
+
+    quotes[currentQuoteIndex].style.display = 'flex';
+    quotes[currentQuoteIndex].classList.add('show');
+
+    setInterval(showNextQuote, 7000);
+});
